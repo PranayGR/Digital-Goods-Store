afcb865b0a9ce8f86193e9cddf98a6b1d75a9908
Pranay <pranayrapartiwar2911@gmail.com>
2022-01-19 19:47:06 +0530

    Added Weather API

---
+++ js/common.js
@@ -0,0 +1,24 @@
+// Geolocation + Weather JS
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else{
+        console.log('Geolocation is Not Supported on this Device');
+    }
+}
+
+showPosition = (data) => {
+    let x = document.getElementById('weatherImage');
+    let y = document.getElementById('temperature');
+    console.log(data.coords.latitude,data.coords.longitude);
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.coords.latitude}&lon=${data.coords.longitude}&mode=json&units=metric&cnt=1&APPID=2b5ab3bc294fc9f57cd2c0e0acaa541a`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data)=> {
+            x.innerHTML = `<img src='http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png' alt='weather'/>`
+            y.innerText = `${data.main.temp}\u00B0C`;
+    })
+}
