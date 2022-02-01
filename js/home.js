4a57b9176cbafc56c3dad408fd40c07cc9656ece
Pranay <pranayrapartiwar2911@gmail.com>
2022-02-01 23:42:11 +0530

    Added Cart and View Btns

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
 // Coupon Open Function
-
 window.addEventListener('load', function(){
     let coupon = document.getElementById('couponPopup');
 
@@ -11,3 +10,31 @@ function closePopup(){
 
     coupon.style.display = 'none';   
 };
+
+
+// Deal Section Timer
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('mins');
+let seconds = document.getElementById('secs');
+
+const countdown = () => {
+    const countDate = new Date("February 10, 2022 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    // how does time work
+    let second = 1000;
+    let min = second * 60;
+    let hour = min * 60;
+    let day = hour*24;
+
+    // Apply Formula for Time
+    days.innerText = ("0" + Math.floor(gap / day)).slice(-2);
+    hours.innerText = ("0" + Math.floor((gap % day) / hour)).slice(-2);
+    minutes.innerText = ("0" + Math.floor((gap % hour) / min)).slice(-2);
+    seconds.innerText = ("0" + Math.floor((gap % min) / second)).slice(-2);
+    
+}
+// To call Function and keep updating every second
+setInterval(countdown, 1000);
